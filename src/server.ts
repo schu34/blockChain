898d7ac3ae329fe7d7c039e346fbb1146c66c706
Matthew Schupack <schu34@gmail.com>
2018-03-24 08:43:18 -0400

    new start script

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,14 @@ import * as WebSocket from "ws";
 
 const expressPort = parseInt(process.env.EPORT);
 const wsPort = parseInt(process.env.WSPORT);
-
+console.log(process.env.EPORT)
 function broadcastLatest() {}
 
 function initHttpServer(port: number) {
 	const app = express();
 	app.use(express.json());
 
-	app.get("/bjjlock", (req, res) => {
+	app.get("/block", (req, res) => {
 		res.send(getBlockchain());
 	});
 
@@ -29,6 +29,14 @@ function initHttpServer(port: number) {
 	});
 
 	app.post("/addPeer", (req, res) => {
+		addPeer(req.body.url);
+		res.sendStatus(200);
+	});
+
+	app.post("/broadcast", (req, res) => {
+		const  data = req.body.data ;
+		console.log(req.body.data)
+		broadcast("broadcasting from " + (wsPort|| 8888) + "\n" + data);
 		res.sendStatus(200);
 	});
 
@@ -37,27 +45,43 @@ function initHttpServer(port: number) {
 	});
 }
 
-
-
-//websockets stuff
-function initWS(port: number){
-	const wss = new WebSocket.Server({port})
-
-	wss.on("connection", (ws)=>{
-		console.log('connection recieved', ws);
-		ws.on("message", (message)=>{
-			console.log("recieved message", message);
-		})
-	})
-
-	return wss;
-}
-
-
 function getSockets() {
 	return [];
 }
 
+function broadcast(data: string) {
+	console.log("going to broadcast", data);
+	(wss.clients as any).forEach(client => {
+	console.log("found client...");
+	if (client.readyState === WebSocket.OPEN) {
+	console.log("client is ready sending data", data);
+			client.send(data);
+		}
+	});
+}
+
+function addPeer(url: string) {
+	const ws = new WebSocket(url);
+	ws.on("message", message => {
+		console.log("message recieved", message);
+	});
+}
+
+//websockets stuff
+function initWS(port: number) {
+	const wss = new WebSocket.Server({ port });
+
+	wss.on("connection", ws => {
+		console.log("connection recieved", ws);
+		ws.on("message", message => {
+			console.log("recieved message", message);
+		});
+	});
+
+	// wss["broadcast"] = message => {};
+
+	return wss;
+}
 
 initHttpServer(expressPort || 8888);
-const wss = initWS(wsPort || 8080);
+const wss = initWS( wsPort || 8080);
